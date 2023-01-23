75d3d4978f4215e7c97330062abdc7e9b885c64d
whiplashz <pilvorm@gmail.com>
2023-01-23 20:17:53 +0100

    Add residual layer

--- src/ResNet.jl
+++ src/ResNet.jl
@@ -12,4 +12,24 @@ function create_basic_block(channels, connection; stride = 1)
     Chain(SkipConnection(before_connection, connection), relu)
 end
 
+function create_layer(block, size, channels; stride = 1)
+    layer = []
+
+    if stride == 1 && channels[1] == channels[2]
+        push!(layer, block(channels, +; stride))
+    else
+        downsample = Chain(
+            Conv((1, 1), channels; stride, bias = False),
+            BatchNorm(channels[2])
+        )
+        push!(layer, block(channels, (x_out, x_in) -> x_out + downsample(x_in); stride))
+    end
+
+    for _ in 2:size
+        push!(layer, block(channels[2] => channels[2], +))
+    end
+
+    Chain(layer...)
+end
+
 end
