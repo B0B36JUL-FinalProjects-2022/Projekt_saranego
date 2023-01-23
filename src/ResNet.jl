6a338f73cd427ebe47a498448de91521c976fd38
whiplashz <pilvorm@gmail.com>
2023-01-23 20:12:50 +0100

    Add basic (residual) block

--- src/ResNet.jl
+++ src/ResNet.jl
@@ -1,5 +1,15 @@
 module ResNet
 
-# Write your package code here.
+using Flux
+
+function create_basic_block(channels, connection; stride = 1)
+    before_connection = Chain(
+        Conv((3, 3), channels; stride, pad = 1, bias = false),
+        BatchNorm(channels[2], relu),
+        Conv((3, 3), channels[2] => channels[2]; pad = 1, bias = false),
+        BatchNorm(channels[2]),
+    )
+    Chain(SkipConnection(before_connection, connection), relu)
+end
 
 end
