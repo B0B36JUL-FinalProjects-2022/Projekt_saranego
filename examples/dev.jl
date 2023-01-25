e6dfc93b1bf142e0891d7e7c14ea59a25c13a7da
whiplashz <pilvorm@gmail.com>
2023-01-25 21:47:16 +0100

    Implement training

--- examples/dev.jl
+++ examples/dev.jl
@@ -1,14 +1,18 @@
 using MLDatasets
 using Random
+using Flux
 using Flux: unsqueeze
+using Plots
 
 using Revise
 using ResNet
 
 Random.seed!(0)
 
-train_x, train_y = FashionMNIST(; Tx=Float32, split=:train)[:]
-test_x, test_y = FashionMNIST(; Tx=Float32, split=:test)[:]
+include("train.jl")
+
+train_x, train_y = FashionMNIST(; Tx=Float32, split=:train)[1:5000]
+test_x, test_y = FashionMNIST(; Tx=Float32, split=:test)[1:1000]
 
 train_x = unsqueeze(train_x, 3)
 test_x = unsqueeze(test_x, 3)
@@ -24,7 +28,29 @@ val_y = test_y[val_indices]
 test_x = selectdim(test_x, 4, test_indices)
 test_y = test_y[test_indices]
 
-rn = RN([1, 4, 8, 16], [2, 2], [2, 2], 10)
+rn = RN([1, 4, 4, 4], [2, 2], [1, 1], 10)
 
-samples = selectdim(train_x, 4, 1:5)
-size(rn(samples))
+epochs = 10
+(train_losses, train_accs), (val_losses, val_accs) = train!(
+    rn, 
+    (train_x, train_y), 
+    (val_x, val_y), 
+    0.01,
+    epochs
+)
+
+plot([train_losses, val_losses];
+    title = "Loss during training",
+    label = ["Training set" "Validation set"],
+    xguide = "Epoch",
+    yguide = "Loss",
+    xticks = 1:epochs,
+)
+
+plot([train_accs, val_accs];
+    title = "Accuracy during training",
+    label = ["Training set" "Validation set"],
+    xguide = "Epoch",
+    yguide = "Accuracy",
+    xticks = 1:epochs,
+)
