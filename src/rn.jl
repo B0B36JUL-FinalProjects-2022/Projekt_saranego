8017110da04bfd18bb9fd1619f41d232c4e50a49
whiplashz <pilvorm@gmail.com>
2023-01-26 21:31:22 +0100

    Add documentation

--- src/rn.jl
+++ src/rn.jl
@@ -4,10 +4,100 @@ struct RN
     entry::Chain
     layers::Chain
     head::Chain
+    n_layers::Integer
 end
 
 Flux.@functor RN
 
+"""
+    RN(; 
+        block,
+        channels::Vector, 
+        strides::Vector, 
+        repeats::Vector, 
+        classes::Integer, 
+        grayscale = false,
+        pooling_dims = (7, 7),
+        kwargs...
+    )
+
+Constructs ResNet, the network which utilizes residual connections to mitigate the
+vanishing gradient problem, make identity functions easier to learn and thus build
+really deep networks.
+
+`block` accepts **BasicBlock** or **Bottleneck**.
+The length of `channels` must be 1 more than the length of `strides` or `repeats`.
+`pooling_dims` defines output spatial dimensions of the final pooling.
+To use `expansion` other than 1 with **Bottleneck** block, pass the corresponding keyword argument.
+Other arguments are self explanatory.
+
+# Example
+
+```julia-repl
+julia> rn = RN(
+    block = BasicBlock,
+    channels = [2, 4, 8], 
+    strides = [2, 2], 
+    repeats = [2, 2], 
+    grayscale = true,
+    classes = 10
+)
+ResNet with 8 layers and 6578 parameters
+
+Entry
+    Conv((3, 3), 1 => 2, pad=1, bias=false)
+    BatchNorm(2, relu)
+Layer1
+    BasicBlock(2 => 4, stride=2)
+    BasicBlock(4 => 4, stride=1)
+Layer2
+    BasicBlock(4 => 8, stride=2)
+    BasicBlock(8 => 8, stride=1)
+Head
+    AdaptiveMeanPool((7, 7))
+    flatten
+    Dense(392 => 10; bias=false)
+    BatchNorm(10)
+    logsoftmax
+
+julia> rn = RN(
+    block = Bottleneck,
+    channels = [32, 64, 128, 256], 
+    strides = [1, 2, 2], 
+    repeats = [4, 4, 4], 
+    grayscale = false,
+    classes = 20,
+    pooling_dims = (5, 5),
+    expansion = 4
+)
+ResNet with 38 layers and 6205384 parameters
+
+Entry
+    Conv((3, 3), 3 => 32, pad=1, bias=false)
+    BatchNorm(32, relu)
+Layer1
+    Bottleneck(32 => 64, stride=1, expansion=4)
+    Bottleneck(256 => 64, stride=1, expansion=4)
+    Bottleneck(256 => 64, stride=1, expansion=4)
+    Bottleneck(256 => 64, stride=1, expansion=4)
+Layer2
+    Bottleneck(256 => 128, stride=2, expansion=4)
+    Bottleneck(512 => 128, stride=1, expansion=4)
+    Bottleneck(512 => 128, stride=1, expansion=4)
+    Bottleneck(512 => 128, stride=1, expansion=4)
+Layer3
+    Bottleneck(512 => 256, stride=2, expansion=4)
+    Bottleneck(1024 => 256, stride=1, expansion=4)
+    Bottleneck(1024 => 256, stride=1, expansion=4)
+    Bottleneck(1024 => 256, stride=1, expansion=4)
+Head
+    AdaptiveMeanPool((5, 5))
+    flatten
+    Dense(25600 => 20; bias=false)
+    BatchNorm(20)
+    logsoftmax
+```
+"""
 function RN(; 
         block,
         channels::Vector, 
@@ -45,14 +135,16 @@ function RN(;
         logsoftmax
     )
 
-    RN(entry, layers, head)
+    layers_per_block = block == BasicBlock ? 2 : 3
+    n_layers = 2 + sum(layers_per_block .* repeats)
+
+    RN(entry, layers, head, n_layers)
 end
 
 (rn::RN)(x) = rn.head(rn.layers(rn.entry(x)))
 
 function Base.show(io::IO, rn::RN)
-    n_layers = length(rn.entry) + length(rn.layers) + length(rn.head)
-    println(io, "ResNet with ", n_layers, " layers and ", number_of_params(rn), " parameters\n")
+    println(io, "ResNet with ", rn.n_layers, " layers and ", number_of_params(rn), " parameters\n")
     
     println(io, "Entry")
     for layer in rn.entry
